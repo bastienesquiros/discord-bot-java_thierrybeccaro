ca6b9319bbc45b043c127d7ad9f85b288edd946e
Bastien Esquiros <bastien.esquiros@hotmail.com>
2023-04-28 21:29:51 +0200

    init: Projet's structure update

--- src/main/java/settings/Initialization.java
+++ src/main/java/settings/Initialization.java
@@ -1,10 +1,10 @@
-package discordbot;
+package settings;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 
-public class Bot {
+public class Initialization {
     public static void main(String[] args) {
         JDA bot = JDABuilder.createDefault("MTEwMTU4MTU3ODAwNzc2MDkzNg.GRa2zD._anOnNiXr6Gv-wGUZspZOEEy273LGuVyMAX9uo")
                 .setActivity(Activity.playing("MOTUS"))
