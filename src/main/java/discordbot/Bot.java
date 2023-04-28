25b2a550179572ad2eb045865c142acda436fba2
Bastien Esquiros <bastien.esquiros@hotmail.com>
2023-04-28 21:12:43 +0200

    init: project's base structure

---
+++ src/main/java/discordbot/Bot.java
@@ -0,0 +1,14 @@
+package discordbot;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+
+public class Bot {
+    public static void main(String[] args) {
+        JDA bot = JDABuilder.createDefault("MTEwMTU4MTU3ODAwNzc2MDkzNg.GRa2zD._anOnNiXr6Gv-wGUZspZOEEy273LGuVyMAX9uo")
+                .setActivity(Activity.playing("MOTUS"))
+                .build();
+
+    }
+}
