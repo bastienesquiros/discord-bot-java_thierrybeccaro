56575699682e6aa09cea161db98d25add37bbb84
Neotep <thibault.altaber@gmail.com>
2023-04-29 21:49:40 +0200

    change

--- src/main/java/settings/Initialization.java
+++ src/main/java/settings/Initialization.java
@@ -11,7 +11,8 @@ import net.dv8tion.jda.api.requests.GatewayIntent;
 public class Initialization {
 
         public static void main(String[] args) throws LoginException, InterruptedException {
-                JDA jda = JDABuilder.createDefault("YOUR_TOKEN_HERE")
+                JDA jda = JDABuilder.createDefault(
+                                "MTEwMTU4MTU3ODAwNzc2MDkzNg.GRa2zD._anOnNiXr6Gv-wGUZspZOEEy273LGuVyMAX9uo")
                                 .enableIntents(GatewayIntent.GUILD_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS,
                                                 GatewayIntent.DIRECT_MESSAGES, GatewayIntent.DIRECT_MESSAGE_REACTIONS,
                                                 GatewayIntent.MESSAGE_CONTENT)
